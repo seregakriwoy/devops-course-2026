675bbcfd81a30eacf0ca29cb01dd988214490e3b
seregakriwoy <sergey.ol.shev@gmail.com>
2026-09-19 18:43:03 +0300

    feat: implement subtract function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 ﻿# Calculator app
 def add(a, b):
     return a + b
-#TODO: add more functions
+#TODO: add more functionsdef subtract(a, b):
+    return a - b # fixed
+def subtract(a, b):
+    return a - b # fixed
